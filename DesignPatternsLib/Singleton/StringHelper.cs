b5832a708b3c6226e8e6de3984413d412752808a
SINGH <vikas.singh.vikas@hp.com>
2020-02-22 17:54:15 +0530

    Implementation of Singleton Pattern

---
+++ DesignPatternsLib/Singleton/StringHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsLib.Singleton
+{
+    public sealed class StringHelper
+    {
+        private static StringHelper _instance = null;
+
+        public string Value { get; private set; }
+        public static StringHelper Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new StringHelper();
+                return _instance;
+            }
+        }
+
+        private StringHelper() { }
+
+        public void Append(string input)
+        {
+            if (!string.IsNullOrEmpty(Value))
+                Value += " " + input;
+            else
+                Value += input;
+        }
+        public void Flush()
+        {
+            Value = string.Empty;
+        }
+
+    }
+}
---
+++ DesignPatternsTest/Singleton/Program.cs
@@ -0,0 +1,38 @@
+﻿using DesignPatternsLib.Singleton;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsTest.Singleton
+{
+    [TestClass]
+    public class Program
+    {
+        static readonly StringHelper _stringHelper;
+        static Program()
+        {
+            _stringHelper = StringHelper.Instance;
+        }
+
+        [TestMethod]
+        public void AppendStringTest()
+        {
+            _stringHelper.Append("Hello");
+            Assert.AreEqual(expected: "Hello",
+                            actual: _stringHelper.Value);
+
+            _stringHelper.Append("World");
+            Assert.AreEqual(expected: "Hello World",
+                            actual: _stringHelper.Value);
+        }
+
+        [TestMethod]
+        public void FlushStringTest()
+        {
+            _stringHelper.Flush();
+            Assert.AreEqual(expected: string.Empty,
+                            actual: _stringHelper.Value);
+        }
+    }
+}
